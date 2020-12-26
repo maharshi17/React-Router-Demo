6dedec69f90074114ddf99ce596ecba11894db95
Maharshi <maharshipandya1999@gmail.com>
2020-12-26 21:08:31 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Services from './components/Services';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/services" component={Services} />
+        <Route path="/About" component={About} />
+        <Route path="/Contact" component={Contact} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Services.js
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom';
+
+const Services = () => {
+    return (
+        <div>
+            <header className="header">
+                <div className="logo">
+                    <a href="/">Logo</a>
+                </div>
+                <nav className="navlinks">
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li className="active"><Link to="/services">Services</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                </nav>
+            </header>
+            <section>
+                <h1>This is a Services page</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nulla commodi eligendi omnis dolorem repellendus illo exercitationem deserunt perspiciatis ipsam eum, recusandae aperiam quod, nesciunt sit corrupti iste laborum laboriosam consequatur atque, fugiat natus. Maxime quod ipsum iure nam. Assumenda eos vitae quaerat facere. Perspiciatis temporibus rerum vero suscipit consequatur cum accusamus! Minima veniam tempora, accusamus animi numquam odio voluptas ab debitis necessitatibus eligendi dolorem nulla quisquam perferendis ducimus sit unde. Vero fugit quae delectus adipisci mollitia quisquam velit eveniet culpa deserunt doloremque, facilis, similique illum porro. Temporibus, doloribus. Similique.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem, sit quia. Architecto facilis, necessitatibus cupiditate cumque est blanditiis hic soluta ipsa, repellendus molestiae laudantium ipsum nulla fugiat rerum eveniet dolorem?</p>
+            </section>
+            <footer>
+                <p>Copyright &copy; 2020 | All rights reserved</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Services;
